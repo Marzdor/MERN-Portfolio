d405880386d452378141e8ffe426f6a3264d08a9
Max Ingersoll <marzdor@gmail.com>
2018-12-26 15:17:33 -0800

    fixxed bugs with old fade system, new system might be over done for what it does

--- client/src/App.js
+++ client/src/App.js
@@ -8,56 +8,12 @@ import Create from "./assests/components/sub_admin/Create";
 import Edit from "./assests/components/sub_admin/Edit";
 import Error from "./assests/components/Error";
 
-const tags = [
-  "JavaScript",
-  "CSS",
-  "HTML",
-  "React",
-  "MongoDB",
-  "Express",
-  "Node.js"
-];
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { textIdx: 0 };
-    this.next = this.next.bind(this);
-  }
-
-  componentDidMount() {
-    // fade effect for tags
-    let currentIdx = this.state.textIdx;
-    document.querySelector(".text-tag--splash").style.opacity = 1;
-    setTimeout(() => {
-      document.querySelector(".text-tag--splash").style.opacity = 0;
-    }, 2150);
-    this.timeout = setInterval(() => {
-      currentIdx = this.state.textIdx;
-      document.querySelector(".text-tag--splash").style.opacity = 1;
-      setTimeout(() => {
-        document.querySelector(".text-tag--splash").style.opacity = 0;
-      }, 2150);
-      this.setState({ textIdx: currentIdx + 1 });
-    }, 3000);
-    //
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timeout);
-  }
-
-  next(currentIdx) {}
-
   render() {
-    let changeTags = tags[this.state.textIdx % tags.length];
     return (
       <Router>
         <Switch>
-          <Route
-            exact
-            path="/"
-            render={props => <Splash tag={changeTags} {...props} />}
-          />
+          <Route exact path="/" component={Splash} />
           <Route path="/work" component={Work} />
           {/* TODO Make admin paths require authentication to access */}
           <Route exact path="/admin" component={Admin} />
--- client/src/assests/components/Splash.js
+++ client/src/assests/components/Splash.js
@@ -1,16 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const Splash = props => {
-  return (
-    <section className="container container-splash ">
-      <h1 className="text-title">Max Ingersoll</h1>
-      <h2 className="text-title-sub">a web dev</h2>
-      <p className="text-tag--splash">{props.tag}</p>
-      <Link className="btn btn-splash" to="/work">
-        View My Work
-      </Link>
-    </section>
-  );
-};
+const tags = [
+  "JavaScript",
+  "CSS",
+  "HTML",
+  "React",
+  "MongoDB",
+  "Express",
+  "Node.js"
+];
+
+class Splash extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textIdx: 0,
+      timeMain: 0,
+      timeSub: 0,
+      isOn: false,
+      op: false
+    };
+
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+  }
+
+  componentDidMount() {
+    // fade effect for tags
+    this.startTimer();
+    //
+  }
+
+  startTimer() {
+    this.setState({
+      isOn: true,
+      timeMain: this.state.timeMain,
+      timeSub: this.state.timeSub,
+      start: Date.now() - this.state.timeMain
+    });
+    this.timerMain = setInterval(
+      () =>
+        this.setState({
+          timeMain: Date.now() - this.state.start,
+          textIdx: this.state.textIdx + 1
+        }),
+      3000
+    );
+    this.timerSub = setInterval(() => {
+      this.setState({
+        timeSub: Date.now() - this.state.start,
+        op: !this.state.op
+      });
+      if (this.state.isOn) {
+        if (this.state.op) {
+          document.querySelector(".text-tag--splash").style.opacity = 0;
+        } else {
+          document.querySelector(".text-tag--splash").style.opacity = 1;
+        }
+      }
+    }, 1500);
+  }
+  stopTimer() {
+    this.setState({ isOn: false, timeMain: 0, timeSub: 0, start: 0 });
+    clearInterval(this.timerMain);
+    clearInterval(this.timerSub);
+  }
+
+  render() {
+    let changeTags = tags[this.state.textIdx % tags.length];
+
+    return (
+      <section className="container container-splash ">
+        <h1 className="text-title">Max Ingersoll</h1>
+        <h2 className="text-title-sub">a web dev</h2>
+        <p className="text-tag--splash">{changeTags}</p>
+        <Link className="btn btn-splash" onClick={this.stopTimer} to="/work">
+          View My Work
+        </Link>
+      </section>
+    );
+  }
+}
 export default Splash;
