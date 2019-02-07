8d907487299dd30aae6bec7c5afc22f8238bc69b
Max Ingersoll <marzdor@gmail.com>
2019-02-07 12:00:03 -0800

    fresh start

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,7 @@ import {
   Redirect
 } from "react-router-dom";
 
-import Splash from "./assests/components/Splash";
-import Work from "./assests/components/Work";
-import Admin from "./assests//components/Admin";
+import Home from "./assests//components/Home";
 import Create from "./assests/components/sub_admin/Create";
 import Edit from "./assests/components/sub_admin/Edit";
 import Error from "./assests/components/Error";
@@ -82,12 +80,11 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Splash} />
-          <Route path="/work" component={Work} />
+          <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
-          <PrivateRoute exact path="/admin" component={Admin} />
-          <PrivateRoute path="/admin/create" component={Create} />
-          <PrivateRoute path="/admin/edit" component={Edit} />
+
+          <Route path="/admin/create" component={Create} />
+          <Route path="/admin/edit" component={Edit} />
           <Route component={Error} />
         </Switch>
       </Router>
---
+++ client/src/assests/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>P</div>;
+};
+
+export default Home;
