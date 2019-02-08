11826676f865bfb82e1f4e1fc823243f49a75ac2
Max Ingersoll <marzdor@gmail.com>
2019-02-07 18:13:07 -0800

    Pulling project data from server

--- client/src/assests/components/Projects.js
+++ client/src/assests/components/Projects.js
@@ -1,38 +1,41 @@
 import React from "react";
 
-const Projects = () => {
-  return (
-    <div>
-      <section>
-        <h2>Site Name</h2>
-        {/* use a list???? or naw */}
-        <ul>
-          <h4>Tech</h4>
-          <li>HTML</li>
-          <li>HTML</li>
-          <li>HTML</li>
-          <li>HTML</li>
-          <li>HTML</li>
-        </ul>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam luctus
-          odio ac sollicitudin tristique. Fusce molestie ipsum non sapien
-          iaculis maximus. Pellentesque sem justo, consequat eu viverra in,
-          hendrerit in lacus. Sed lacinia augue at suscipit tristique. Mauris
-          nec ipsum sit amet tortor semper consectetur. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus. Donec a quam nec erat
-          tristique bibendum sit amet vel nunc. Aliquam porta consectetur nulla
-          ut lacinia.
-        </p>
-        <a href="/" target="_blank" rel="noopener noreferrer">
+const Projects = props => {
+  // Create individual site elements
+  const siteElements = [];
+  for (let site in props.sites) {
+    // Create tech elements
+    const techElements = [];
+    const techList = props.sites[site].tech;
+    for (let tech in techList) {
+      techElements.push(<li key={techList[tech]}>{techList[tech]}</li>);
+    }
+
+    siteElements.push(
+      <section key={props.sites[site].name}>
+        <h2>{props.sites[site].name}</h2>
+        <h4>Tech</h4>
+        <ul>{techElements}</ul>
+        <p>{props.sites[site].description}</p>
+        <a
+          href={props.sites[site].urls.code}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Code
         </a>
-        <a href="/" target="_blank" rel="noopener noreferrer">
+        <a
+          href={props.sites[site].urls.live}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Live
         </a>
       </section>
-    </div>
-  );
+    );
+  }
+
+  return <div>{siteElements}</div>;
 };
 
 export default Projects;
--- client/src/assests/components/Work.js
+++ client/src/assests/components/Work.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Header from "./Header";
 import Projects from "./Projects";
 
-const Work = () => {
-  return (
-    <div>
-      <Header title={" - Projects"} />
-      <Projects />
-    </div>
-  );
-};
+class Work extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+  componentDidMount() {
+    fetch("api/projects")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ sites: data, isLoading: false });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <Header title={"Projects"} />
+        {!this.state.isLoading && <Projects sites={this.state.sites} />}
+      </div>
+    );
+  }
+}
 
 export default Work;
