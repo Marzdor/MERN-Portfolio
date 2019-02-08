e26cd902f78844eb0e9b1c01ac1c12b537bc8419
Max Ingersoll <marzdor@gmail.com>
2019-02-07 21:17:04 -0800

    Removed filler text

--- client/src/assests/components/Home.js
+++ client/src/assests/components/Home.js
@@ -7,23 +7,21 @@ const Home = () => {
   return (
     <div>
       <Header title={""} />
-      <section>
+      <section className="home">
+        <h2>About</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam luctus
-          odio ac sollicitudin tristique. Fusce molestie ipsum non sapien
-          iaculis maximus. Pellentesque sem justo, consequat eu viverra in,
-          hendrerit in lacus. Sed lacinia augue at suscipit tristique. Mauris
-          nec ipsum sit amet tortor semper consectetur. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus. Donec a quam nec erat
-          tristique bibendum sit amet vel nunc. Aliquam porta consectetur nulla
-          ut lacinia. Nam eu lorem quis libero posuere facilisis in sed lorem.
-          Nulla tristique ullamcorper ultrices. Donec vehicula libero sit amet
-          augue dapibus scelerisque. Donec non tincidunt elit. Donec non est sed
-          nisl molestie dapibus nec et lorem. Donec mattis, quam nec iaculis
-          venenatis, nunc felis auctor neque, vel ullamcorper tellus nisi ac
-          sem. Integer luctus ultricies nulla ac viverra.
+          I'm a "loosely used" Full-Stack Developer, with less then 1 year
+          hobbyist experience. Looking to build that “professional experience”
+          that everyone is talking about.
         </p>
-        <Link to="/work">View My Projects</Link>
+        <p>
+          I’m currently working toward a Bachelors Degree in Computer Science.
+          while I continue to build my skills in the the web development
+          ecosystem.
+        </p>
+        <Link className="btn" to="/work">
+          View My Projects
+        </Link>
       </section>
     </div>
   );
